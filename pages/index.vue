08cb94fd681ecdffd19cb3d7791c5503efe529e4
PPantech <alsgur0008@naver.com>
2018-07-05 08:25:29 +0900

    완성

--- pages/index.vue
+++ pages/index.vue
@@ -5,60 +5,60 @@
       <div @click="viewPage">
         <h1 class="title">실시간</h1>
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/main.jpg" alt="img" class="sommail" style="width: 80px;height: 80px;margin-bottom: -30px;margin-left: -10px;margin-right: 20px;">
         </div>
-        <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
-        </div>
-      </div>
-    </div>
-    <div class="list">
-      <div @click="viewPage">
-        <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
-        </div>
-        <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+        <div class="list-content" style="position: relative;bottom: -10px;">
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade2.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade3.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade4.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
+        </div>
+      </div>
+    </div>
+    <div class="list">
+      <div @click="viewPage">
+        <div class="list-img">
+          <img src="../assets/image/trade5.png" alt="img" class="somnail">
+        </div>
+        <div class="list-content">
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
@@ -69,60 +69,60 @@
       <div @click="viewPage">
         <h1 class="title">스크랩</h1>
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade2.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade3.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade1.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade4.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade5.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
@@ -133,60 +133,60 @@
       <div @click="viewPage">
         <h1 class="title">찜 </h1>
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade4.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade2.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade3.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade1.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
     <div class="list">
       <div @click="viewPage">
         <div class="list-img">
-          <img src="../assets/image/logo.png" alt="img" class="somnail">
+          <img src="../assets/image/trade5.png" alt="img" class="somnail">
         </div>
         <div class="list-content">
-          <p class="c-title">제목이당</p>
-          <p class="c-content">내용인데요 그러허러내용인데요 그러허러내용인데요 그러허러...</p>
-          <p class="c-info">작성자: 김모씨</p>
+          <p class="c-title">물건 팝니다</p>
+          <p class="c-content">사진에 있는 물건 팝니다. 네고 사절합니다 안전거래 희망합니다</p>
+          <p class="c-info">작성자: XXX</p>
         </div>
       </div>
     </div>
